c6a997f8374a665b38629de7fb0bb76d41820248
armando01ramirez <azb619@hotmail.com>
2021-05-29 14:38:10 -0500

    unidad 2

---
+++ react-props-crud-master/src/App.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import UserTable from './componentes/UserTable';
+import { v4 as uuidv4 } from 'uuid';
+import AddUserForm from './componentes/AddUserForm';
+import EditUserForm from './componentes/EditUserForm';
+
+function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+    { id: uuidv4(), name: 'Lupita', username: 'lupita_00'}
+  ]
+
+  //Nota simpre que usemos useState hay que ver que se este agregando en la lib de react
+  const [users, setUsers] = useState(usersData);
+  
+  //Agregar usuarios
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+
+  //Eliminar usuarios
+  const deleteUser = (id) => {
+    console.log(id)
+    //Condicion para sobre escribir el arreglo
+    setUsers(users.filter(user => user.id != id))
+  }
+
+  //Editar usuario
+  const [bandera, setbandera] = useState(false);
+  const [currentUser, setcurrentUser] = useState({
+    id: null,
+    name: '',
+    username: ''
+  });
+
+  const editRow = (user) =>{
+    setbandera(true);
+    setcurrentUser({
+      id: user.id, name: user.name, username: user.username
+    })
+  }
+
+  const updateUser = (id, updateUser) => {
+    setbandera(false);
+    setUsers(users.map(user => (user.id === id ? updateUser : user)))
+  }
+
+  return (
+    <div className="container">
+      <h1>Proyecto UNIDAD 2 CRUD</h1>
+      <div className="flex-row">
+      <div className="flex-large">
+          {
+            bandera ? (
+            <div>
+              <h2>Editar</h2>
+              <EditUserForm currentUser={currentUser} updateUser={updateUser}/>
+            </div>
+            ) : (
+              <div>
+                <h2>agregar usuario</h2>
+                <AddUserForm addUser={addUser}/>
+              </div>
+            )
+          }
+        </div>
+        <div className="flex-large">
+          <h2>Visualizar</h2>
+          <UserTable 
+          users={users} 
+          deleteUser={deleteUser} 
+          editRow={editRow}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
